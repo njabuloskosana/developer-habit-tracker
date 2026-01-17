9f6184347bba0fb5b18bd20063dfb2f8c81f0ad8
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2026-01-17 18:55:55 +0200

    Submitting Progress : Project Setup
    - .NET 9 WebApi
    - OpenTelementary monitoring with aspire dashboard
    - Docker ochastration
    - build piplines

---
+++ DevHabit/DevHabit.Api/Program.cs
@@ -0,0 +1,41 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    .WithTracing(tracing => tracing
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation())
+    .WithMetrics(metrics => metrics
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation()
+    .AddRuntimeInstrumentation())
+    .UseOtlpExporter();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeScopes = true;
+    options.IncludeFormattedMessage = true;
+});
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+await app.RunAsync();
