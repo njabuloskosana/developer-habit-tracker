97e539666beb19a0c842d01c1de1674bacc0025b
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2026-02-22 09:27:24 +0200

    Fixing get projection
    Fixing get projection

--- DevHabit/DevHabit.Api/Controllers/HabitsController.cs
+++ DevHabit/DevHabit.Api/Controllers/HabitsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using DevHabit.Api.Database;
 using DevHabit.Api.Entities;
 using Microsoft.EntityFrameworkCore;
+using DevHabit.Api.DTOs.Habits;
 
 namespace DevHabit.Api.Controllers;
 
@@ -13,7 +14,34 @@ public class HabitsController(ApplicationDbContext dbContext) : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetHabits()
     {
-        List<Habit> habits = await dbContext.Habits.ToListAsync();
+        List<HabitDto> habits = await dbContext.Habits.Select(h=>
+        new HabitDto{
+            Id = h.Id,
+            Name = h.Name,
+            Description = h.Description,
+            Type = (DTOs.Habits.HabitType)h.Type,
+            Frequency = new FrequencyDto
+            {
+                Type = (DTOs.Habits.FrequencyType)h.Frequency.Type,
+                TimesPerPeriod = h.Frequency.TimesPerPeriod
+            },
+            Target = new TargetDto
+            {
+                Value = h.Target.Value,
+                Unit = h.Target.Unit
+            },
+            Status = (DTOs.Habits.HabitStatus)h.Status,
+            IsArchived = h.IsArchived,
+            EndDate = h.EndDate,
+            Milestone = h.Milestone == null ?null : new MilestoneDto
+            {
+                Target = h.Milestone.Target,
+                Current = h.Milestone.Current
+            },
+            CreatedAtUtc = h.CreatedAtUtc,
+           UpdatedAtUtc = h.UpdatedAtUtc,
+           LastCompletedAtUtc = h.LastCompletedAtUtc
+            }).ToListAsync();
         return Ok(habits);
     }
 }
